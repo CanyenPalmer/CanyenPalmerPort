66a82524b172f0d52a4469980950f090b3c54b5e
Canyen Palmer <Canyen2019@gmail.com>
2025-08-21 15:37:17 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./pages/**/*.{ts,tsx}"],
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: { extend: { borderRadius: { '2xl': '1.25rem' } } },
   plugins: [],
 };
-
